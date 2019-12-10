603e064403dcc3aee09d594e78fbe879abf2ae1d
raja <sainiraja59@gmail.com>
2019-12-10 12:43:49 +0530

    register from ready

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+  gotoLogin(){
+    this.router.navigateByUrl('login')
+  }
+  registerForm=new FormGroup({
+    fname:new FormControl(''),
+    lname:new FormControl(''),
+    password:new FormControl(''),
+    rpassword:new FormControl(''),
+    email:new FormControl(''),
+    phone:new FormControl(''),
+    question:new FormControl(''),
+    answer:new FormControl(''),
+    gender:new FormControl(''),
+  })
+  registerFormSubmit(r){
+    console.log(r);
+    this.registerForm.reset()
   }
 
 }
