c5556dcfe0b4c242d0eac5e7be95eef0868a8615
raja <sainiraja59@gmail.com>
2019-12-10 12:26:46 +0530

    login compoenent ui done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+  form: FormGroup = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  });
+  submit(r) {
+    if (this.form.valid) {
+      console.log(r);
+    }
+  }
+  gotoRegister(){
+    this.router.navigateByUrl('register');
   }
 
 }
