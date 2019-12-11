3b936cf337f34f7ea1d4cd04175da5ac05e2e93b
raja <sainiraja59@gmail.com>
2019-12-11 12:49:50 +0530

    subcategory started

--- src/app/admin/subcategory/subcategory.component.ts
+++ src/app/admin/subcategory/subcategory.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-subcategory',
@@ -11,5 +12,9 @@ export class SubcategoryComponent implements OnInit {
 
   ngOnInit() {
   }
+  SubCategoryForm=new FormGroup({
+    name:new FormControl(''),
+    description:new FormControl('')
+  })
 
 }
