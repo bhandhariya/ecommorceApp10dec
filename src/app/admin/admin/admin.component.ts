58d7c3e5f2502eb8caf2258609ac99dfedd1db38
raja <sainiraja59@gmail.com>
2019-12-11 12:11:05 +0530

    logout done

--- src/app/admin/admin/admin.component.ts
+++ src/app/admin/admin/admin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
@@ -8,11 +8,16 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class AdminComponent implements OnInit {
   username;email;
-  constructor(private aroute:ActivatedRoute) { }
+  constructor(private aroute:ActivatedRoute,private router:Router) { }
 
   ngOnInit() {
    this.username=localStorage.getItem('username');
    this.email=localStorage.getItem('email');
+  }
+  logout(){
+    localStorage.removeItem('username');
+    localStorage.removeItem('email');
+    this.router.navigateByUrl('/login')
   }
 
 }
