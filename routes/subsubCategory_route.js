17ca2683ed301da87b9b4e9525f8737a09e8687c
raja <sainiraja59@gmail.com>
2019-12-12 11:21:56 +0530

    createSScategory api created for ceateing new subsubcategory

--- routes/subsubCategory_route.js
+++ routes/subsubCategory_route.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var SSC=require('../controller/subsubCategory_controller');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send({ title: 'Sub Sub Category workling fine' });
 });
+
+router.post('/createSScategory',SSC.createSubSUbCategory);
 
 module.exports = router;
