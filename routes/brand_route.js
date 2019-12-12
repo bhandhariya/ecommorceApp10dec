58cb3ad04c29489642ea91e8d65f50334556ea79
raja <sainiraja59@gmail.com>
2019-12-12 12:59:03 +0530

    create api created

--- routes/brand_route.js
+++ routes/brand_route.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var Brand=require('../controller/brand_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render({ title: 'Brnad api working' });
 });
+
+router.post('/create',Brand.createBrand);
 
 module.exports = router;
