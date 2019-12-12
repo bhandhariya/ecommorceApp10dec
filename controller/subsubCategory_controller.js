3f9c6e2e7065d53eb9fe185e1f22a4fa46c77ee3
raja <sainiraja59@gmail.com>
2019-12-12 11:31:44 +0530

    create subsubcategory function created

--- controller/subsubCategory_controller.js
+++ controller/subsubCategory_controller.js
@@ -1,2 +1,22 @@
 var SubSubCategory=require('../model/subSubCategory_model');
 var mongoose=require('mongoose');
+
+exports.createSubSUbCategory=function(req,res,next){
+    var data=req.body;
+    console.log(data);
+    var obj=new SubSubCategory({
+        _id:new mongoose.Types.ObjectId,
+        name:data.name,
+        description:data.description,
+        creationDate:Date.now(),
+        categoryid:data.categoryid,
+        subcategoryid:data.subcategoryid
+    });
+    obj.save(function(err,ssc){
+        if(ssc){
+            res.send(ssc);
+        }else{
+            res.send(err);
+        }
+    })
+}
